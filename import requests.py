9413fc2dfbe5b614b96582b9b2dbb10bb5f95b95
tbernard-ITQ <tbernard@itq.eu>
2024-03-24 16:38:16 +0100

    delete

--- import requests.py
+++ import requests.py
@@ -1,29 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Remplacez 'URL_DU_SITE_WEB' par l'URL de la page contenant le prix
-url = 'https://www.coolblue.be/fr/produit/888735/jura-z10-aluminium-black-ea.html'
+# Remplacez 'CHEMIN_DU_PILOTE' par le chemin de votre pilote Chrome
+service = Service(executable_path='/usr/local/bin/chromedriver')
 
-# Utilisez Requests pour obtenir le contenu de la page
-response = requests.get(url)
+# Initialisez le navigateur avec l'instance de service
+navigateur = webdriver.Chrome(service=service)
 
-# Vérifiez si la requête a réussi
-if response.status_code == 200:
-    # Utilisez Beautiful Soup pour analyser le contenu HTML
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Remplacez 'SELECTEUR_CSS' par le sélecteur CSS que vous avez trouvé
-    # Par exemple, '.price' ou '#product-price'
-    selecteur_css = 'sales-price__current'
-    
-    # Utilisez le sélecteur CSS pour trouver l'élément contenant le prix
-    element_prix = soup.select_one(selecteur_css)
-    
-    # Vérifiez si l'élément a été trouvé
-    if element_prix:
-        # Affichez le texte de l'élément, qui devrait être le prix
-        print("Prix trouvé :", element_prix.text.strip())
-    else:
-        print("L'élément correspondant au sélecteur CSS n'a pas été trouvé.")
-else:
-    print("La requête a échoué avec le code d'état :", response.status_code)
+try:
+    # Votre code pour ouvrir l'URL et extraire les données
+    navigateur.get('https://www.coolblue.be/fr/produit/888735/jura-z10-aluminium-black-ea.html')
+    element_prix = WebDriverWait(navigateur, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".sales-price__current"))
+    )
+    print("Prix trouvé :", element_prix.text)
+finally:
+    navigateur.quit()
